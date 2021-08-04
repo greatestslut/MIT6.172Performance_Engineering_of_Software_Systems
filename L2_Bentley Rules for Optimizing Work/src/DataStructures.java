2bbbcbcf3fef32c9010bd55c932e27e7354f75e5
bryan <bestbryan13@gmail.com>
2021-08-04 13:43:46 +0800

    add more notes

--- L2_Bentley Rules for Optimizing Work/src/DataStructures.java
+++ L2_Bentley Rules for Optimizing Work/src/DataStructures.java
@@ -1,8 +1,13 @@
 public class DataStructures {
+  //The work of a program (on a given input) is
+  //the sum total of all the operations executed
+  //by the program
+
   //1.Packing and Encoding
   //2.Augmentation add information to interface
   //3.precomputation e.g. map record to object, just compute the location once
   //4.caching
   //5.sparsity e.g.Compressed Sparse Row
+  //6.lazy evaluation(Stream Programming) [decouple thr order of events in our program and how we run them on machine]
 
 }
